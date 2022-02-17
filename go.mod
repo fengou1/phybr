d4b4a6fa402fcdb6e8bf2cdb902622543d14606e
qupeng <qupeng@pingcap.com>
2022-02-17 17:42:59 +0800

    it seems can work now
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hicqu/phybr
 go 1.16
 
 require (
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.5.2
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/emirpasic/gods/maps/treemap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
@@ -16,18 +17,26 @@ import (
 )
 
 type X struct {
-	instances   int
-	offset      *uint32
-	received    *sync.WaitGroup
-	regionMetas [][]*pb.RegionMeta
+	instances      int
+	offset         *uint32
+	received       *sync.WaitGroup // Whether all reports are collected or not.
+	generated      *sync.WaitGroup // Whether commands are generated or not.
+	finished       *sync.WaitGroup // Whether all commands are sent out or not.
+	regionMetas    [][]*pb.RegionMeta
+	regionRecovers [][]*pb.RegionRecover
 }
 
 func newX(instances int) X {
 	var offset = new(uint32)
 	var received = new(sync.WaitGroup)
 	received.Add(instances)
+	var generated = new(sync.WaitGroup)
+	generated.Add(1)
+	var finished = new(sync.WaitGroup)
+	finished.Add(instances)
 	var regionMetas = make([][]*pb.RegionMeta, instances)
-	return X{instances, offset, received, regionMetas}
+	var regionRecovers = make([][]*pb.RegionRecover, instances)
+	return X{instances, offset, received, generated, finished, regionMetas, regionRecovers}
 }
 
 func (x X) RecoverRegions(stream pb.Phybr_RecoverRegionsServer) (err error) {
@@ -43,15 +52,24 @@ func (x X) RecoverRegions(stream pb.Phybr_RecoverRegionsServer) (err error) {
 			return err
 		}
 	}
-	fmt.Printf("receive reports from one instance finish\n")
-	// Do somethine here...
+	fmt.Printf("receive reports from instance %d finish\n", offset)
 	x.received.Done()
+
+	x.generated.Wait()
+	for _, command := range x.regionRecovers[offset] {
+		if err = stream.Send(command); err != nil {
+			fmt.Errorf("send fail: %v\n", err)
+			return err
+		}
+	}
+	fmt.Printf("all commands are sent to instance %d\n", offset)
+	x.finished.Done()
 	return
 }
 
 func main() {
 	address := "127.0.0.1:3379"
-	var x = newX(2)
+	var x = newX(4)
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
@@ -72,16 +90,29 @@ func main() {
 
 	// Wait for all TiKV instances reporting region metas.
 	x.received.Wait()
-	selectReplicas(x.regionMetas)
+	selectReplicas(x.regionMetas, x.regionRecovers)
+	fmt.Printf("commands are generated\n")
+	x.generated.Done()
 
+	x.finished.Wait()
 	fmt.Printf("stopping...")
 	s.Stop()
 }
 
-func selectReplicas(regionMetas [][]*pb.RegionMeta) [][]*pb.RegionRecover {
+func selectReplicas(regionMetas [][]*pb.RegionMeta, commands [][]*pb.RegionRecover) {
 	type M struct {
 		*pb.RegionMeta
 		offset int
+	}
+
+	type P struct {
+		r uint64
+		v uint64
+	}
+
+	type T struct {
+		k []byte
+		r uint64
 	}
 
 	// Group region metas by region id.
@@ -94,11 +125,117 @@ func selectReplicas(regionMetas [][]*pb.RegionMeta) [][]*pb.RegionRecover {
 			regions[m.RegionId] = append(regions[m.RegionId], M{m, offset})
 		}
 	}
-	// Reverse sort replicas by applied index.
-	for rid := range regions {
-		var x = regions[rid]
+
+	// Reverse sort replicas by applied index, and collect all regions' version.
+	var versions = make([]P, 0, len(regions))
+	for r, x := range regions {
 		sort.Slice(x, func(i, j int) bool { return x[i].AppliedIndex > x[j].AppliedIndex })
+		var v = x[0].Version
+		versions = append(versions, P{r, v})
+	}
+	sort.Slice(versions, func(i, j int) bool { return versions[i].v > versions[j].v })
+
+	// Resolve version conflicts.
+	var topo = treemap.NewWith(keyCmpInterface)
+	for _, p := range versions {
+		var sk = prefixStartKey(regions[p.r][0].StartKey)
+		var ek = prefixEndKey(regions[p.r][0].EndKey)
+		fk, fv := topo.Floor(sk)
+		if fk == nil || keyEq(fk.([]byte), sk) || keyCmp(fv.(T).k, ek) > 0 {
+			topo.Put(ek, T{sk, p.r})
+		}
 	}
 
-	return nil
+	// After resolved, all reserved regions shouldn't be tombstone.
+	var reserved = make(map[uint64]struct{}, 0)
+	iter := topo.Iterator()
+	for iter.Next() {
+		v := iter.Value().(T)
+		if regions[v.r][0].Tombstone {
+			fmt.Printf("reserved region shouldn't be tombstone: %d\n", v.r)
+			panic("reserved region shouldn't be tombstone")
+		}
+		reserved[v.r] = struct{}{}
+	}
+
+	// Generate recover commands.
+	for r, x := range regions {
+		if _, ok := reserved[r]; !ok {
+			// Generate a tombstone command.
+			for _, m := range x {
+				cmd := &pb.RegionRecover{RegionId: m.RegionId, Tombstone: true}
+				if cmd.Tombstone != m.Tombstone {
+					commands[m.offset] = append(commands[m.offset], cmd)
+				}
+			}
+		} else {
+			// Generate normal commands.
+			var maxTerm uint64 = 0
+			for _, m := range x {
+				if m.Term > maxTerm {
+					maxTerm = m.Term
+				}
+			}
+			for i, m := range x {
+				cmd := &pb.RegionRecover{RegionId: m.RegionId, Term: maxTerm}
+				cmd.Silence = i > 0
+				if cmd.Term != m.Term || cmd.Silence {
+					commands[m.offset] = append(commands[m.offset], cmd)
+				}
+			}
+		}
+	}
+}
+
+func prefixStartKey(key []byte) []byte {
+	var sk = make([]byte, 0, len(key)+1)
+	sk = append(sk, 'z')
+	sk = append(sk, key...)
+	return sk
+}
+
+func prefixEndKey(key []byte) []byte {
+	if len(key) == 0 {
+		return []byte{'z' + 1}
+	}
+	return prefixStartKey(key)
+}
+
+func keyEq(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func keyCmp(a, b []byte) int {
+	var length = 0
+	var chosen = 0
+	if len(a) < len(b) {
+		length = len(a)
+		chosen = -1
+	} else if len(a) == len(b) {
+		length = len(a)
+		chosen = 0
+	} else {
+		length = len(b)
+		chosen = 1
+	}
+	for i := 0; i < length; i++ {
+		if a[i] < b[i] {
+			return -1
+		} else if a[i] > b[i] {
+			return 1
+		}
+	}
+	return chosen
+}
+
+func keyCmpInterface(a, b interface{}) int {
+	return keyCmp(a.([]byte), b.([]byte))
 }
